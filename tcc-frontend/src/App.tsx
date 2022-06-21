0bce4bf669d4bd9215b5bc2b1b068573b6dceebd
Danilo Medeiros <danilo.medeiros@splab.ufcg.edu.br>
2022-06-21 18:03:36 -0300

    added initial router

--- tcc-frontend/src/App.tsx
+++ tcc-frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import { Navigate } from "react-router";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
