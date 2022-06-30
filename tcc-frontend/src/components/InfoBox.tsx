f8df9094f0ddff7f1b22b629c9d17dedc00f6824
Danilo Medeiros <danilo.medeiros@splab.ufcg.edu.br>
2022-06-30 09:34:24 -0300

    fixed spacing & updated infobox color

--- tcc-frontend/src/components/InfoBox.tsx
+++ tcc-frontend/src/components/InfoBox.tsx
@@ -6,7 +6,7 @@ interface InfoBoxProps {
 function InfoBox(props: InfoBoxProps) {
   return (
     <div className="flex items-center justify-center w-screen mt-10 mb-4">
-      <div className="flex flex-col items-center justify-center w-3/4 bg-zinc-500 rounded-[20px] p-3 text-center">
+      <div className="flex flex-col items-center justify-center w-3/4 bg-cerGreen rounded-[20px] p-3 text-center">
         <h2 className="font-grandstander text-black">{props.title}</h2>
         <p>{props.content}</p>
       </div>
