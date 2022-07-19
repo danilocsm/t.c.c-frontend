f810b6481973c6509b889287699f5457ee412118
Danilo Medeiros <danilo.medeiros@splab.ufcg.edu.br>
2022-07-19 15:25:28 -0300

    fixed home page responsiveness

--- tcc-frontend/src/components/SectionContainer.tsx
+++ tcc-frontend/src/components/SectionContainer.tsx
@@ -14,11 +14,11 @@ function SectionContainer({ name, brief, page }: SectionContainerProps) {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center rounded-[20px] bg-cerGreen w-[calc(70%-2rem)] h-[calc(25vh-2rem)] px-2 gap-y-2">
-      <p className="text-[20px] text-center lg:block hidden">{brief}</p>
+    <div className="flex flex-col items-center justify-center rounded-[20px] bg-cerGreen w-full md:w-[calc(70%-2rem)] h-[calc(30vh-2rem)] md:px-2 md:gap-y-2">
+      <p className="2xl:text-[20px] md:flex justify-center items-center text-center hidden text-[16px] md:h-[calc(65%-1rem)]">{brief}</p>
       <button
         onClick={onButtonClick}
-        className="flex items-center justify-center bg-cerPurple rounded-[20px] w-[calc(50%-2rem)] h-[calc(25%-1rem)] hover:bg-cerBlue focus:outline-none focus:bg-cerBlue focus:border-[1px] focus: border-white"
+        className="flex items-center justify-center bg-cerPurple rounded-[20px] w-full h-full md:w-[calc(50%-2rem)] md:h-[calc(35%-1rem)] hover:bg-cerBlue focus:outline-none focus:bg-cerBlue focus:border-[1px] focus: border-white"
       >
         {name}
       </button>
